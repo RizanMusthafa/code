a090ea1f85ff8e731eff061bb467ab83d75c1dbc
Rizan musthafa <musthafarizan@gmail.com>
2018-10-19 03:24:15 +0530

    explain functional react components

--- a.js
+++ a.js
@@ -1,3 +1,11 @@
 This is my first commit 
 Added in dev branch
 Added in dev branch2
+
+// how to create a functional component in react
+
+import React from 'react';
+
+const Header = (props) => (
+  <header>This is the header of the app</header>
+)
